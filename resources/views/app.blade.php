3b3eaf2aaf0d49fa6ff64d540f30de1f1ffa1998
Mubashir Masoodi <53052123+masoodimubashir@users.noreply.github.com>
2025-06-04 16:37:08 +0530

    ...

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,31 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
-
     @include('layout.head')
-    
-
     @include('layout.css')
 
     @routes
-
     @viteReactRefresh
-
-    @vite(['public/assets/scss/style.scss'])
-
-    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
-
+    @vite(['resources/css/app.css', 'resources/scss/style.scss', 'resources/js/app.jsx'])
     @inertiaHead
-
 </head>
-
 <body class="font-sans antialiased">
     @inertia
-
     @include('layout.script')
-
-
 </body>
-
 </html>
--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,20 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'resources/css/app.css',
+                'resources/scss/style.scss',
+                'resources/js/app.jsx'
+            ],
             refresh: true,
         }),
         react(),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import']
+            },
+        }
+    }
 });
-
-
